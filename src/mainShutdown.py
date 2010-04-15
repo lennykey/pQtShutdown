294f7e0dc53df81d4f9eebe248ff6a20714c3047
jupiter <jupiter@jupiter-desktop>
2010-04-15 09:21:06 +0200

    Update: Sec

--- src/mainShutdown.py
+++ src/mainShutdown.py
@@ -4,10 +4,10 @@ from CShutdown import Shutdown
 
 def main():
     myShutdown= Shutdown()
-    #myShutdown.startShutdown('-P', '45', 'diesspw7')
+    #myShutdown.startShutdown('-P', '45', 'password')
     pass
     pass
-    myShutdown.stopShutdown("Shutdown aufgehoben", 'diesspw7')
+    myShutdown.stopShutdown("Shutdown aufgehoben", 'password')
 
 
 main()
